3106cbcfad7d261b6fd2feab845b94b1d283aef2
Sergei Shcherbina <serggyserggy95@gmail.com>
2026-04-19 23:20:22 +0500

    Фронтендеры вперед гойда

---
+++ front.sql
@@ -0,0 +1,4 @@
+-- полезные скрипты для фронтендеров --
+
+-- для проверки переставленных точек и добавления в ключевую бд --
+SELECT id, name, x_coord, y_coord FROM points WHERE building_id = 10 AND floor_id = 33;
