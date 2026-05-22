986f4c667fa9a42146f7d4f2db211438059f415f
VictoriaOshchepkova <viktoshepkova@gmail.com>
2026-05-22 20:36:56 +0500

    переходы из 12

--- SQL/btw.sql
+++ SQL/btw.sql
@@ -3,5 +3,6 @@ INSERT INTO edges (id, from_point_id, to_point_id, distance_meters, direction_te
 (715014, 702139, 602007, 12, '', true),
 (715016, 702139, 802001, 28, '', true),
 (715017, 101997, 801060, 60, '', true),
-(715021, 802001, 602007, 25, '', true)
+(715021, 802001, 602007, 25, '', true),
+(1011000, 101002, 1202099, 5, '', true)
 ON CONFLICT (id) DO NOTHING;
